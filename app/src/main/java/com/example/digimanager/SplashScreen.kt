c8786f962db51ece70ea426cbffde9d5fc942dce
OxVidhi <81790585+Bhawna1203@users.noreply.github.com>
2022-05-31 22:25:27 +0530

    splash screen animation done

--- app/src/main/java/com/example/digimanager/SplashScreen.kt
+++ app/src/main/java/com/example/digimanager/SplashScreen.kt
@@ -1,11 +1,30 @@
 package com.example.digimanager
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import android.widget.ImageView
+import android.view.animation.AnimationUtils
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        val sideAnimation = AnimationUtils.loadAnimation(this,R.anim.slide)
+        val backgroundImg:ImageView = findViewById(R.id.iv_logo)
+        backgroundImg.startAnimation(sideAnimation)
+
+        Handler().postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        },2000)
     }
 }
