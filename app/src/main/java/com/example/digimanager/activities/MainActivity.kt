bdfc2efe094d91aad5c1ba653833556cae8759ee
Bhawna <bhawnabc2002@gmail.com>
2022-06-11 21:41:04 +0530

    working on digi

--- app/src/main/java/com/example/digimanager/activities/MainActivity.kt
+++ app/src/main/java/com/example/digimanager/activities/MainActivity.kt
@@ -3,10 +3,12 @@ package com.example.digimanager.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.digimanager.R
+import kotlinx.android.synthetic.main.app_bar_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    
 }
