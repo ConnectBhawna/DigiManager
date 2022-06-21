5cc364a4d1c2f90df9d0aa9ac37b52d5b4f05692
Bhawna <bhawnabc2002@gmail.com>
2022-06-21 11:44:39 +0530

    Profile data from firebase

--- app/src/main/java/com/example/digimanager/activities/MyProfileActivity.kt
+++ app/src/main/java/com/example/digimanager/activities/MyProfileActivity.kt
@@ -1,15 +1,36 @@
 package com.example.digimanager.activities
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.content.pm.PathPermission
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.ActionMode
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.example.digimanager.R
 import com.example.digimanager.firestore.FirestoreClass
 import com.example.digimanager.models.User
 import kotlinx.android.synthetic.main.activity_my_profile.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+import java.io.IOException
+
 
 class MyProfileActivity : BaseActivity() {
+
+    companion object {
+        private const val READ_STORAGE_PERMISSION_CODE =1
+        private const val PICK_IMAGE_REQUEST_CODE = 2
+    }
+
+    private var mselectedImageFileUri: Uri?= null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_profile)
@@ -17,6 +38,72 @@ class MyProfileActivity : BaseActivity() {
         setupActionBar()
 
         FirestoreClass().loadUserData(this)
+
+        iv_profile_user_image.setOnClickListener{
+
+            if(ContextCompat.checkSelfPermission(
+                    this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_GRANTED){
+                showImageChooser()
+
+            }
+            else{
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    READ_STORAGE_PERMISSION_CODE
+                )
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode : Int,
+        permission: Array<out String>,
+        grantResults: IntArray
+    ){
+        super.onRequestPermissionsResult(requestCode, permission, grantResults)
+        if(requestCode == READ_STORAGE_PERMISSION_CODE){
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                showImageChooser()
+            }
+            else{
+                Toast.makeText(
+                    this,
+                    "Oops, you just denied the permission for storage. You can allow it from settings.",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+
+    }
+
+    private fun showImageChooser(){
+        var galleryIntent = Intent(Intent.ACTION_PICK,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(galleryIntent, PICK_IMAGE_REQUEST_CODE)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK
+            && requestCode == 0
+            && data!!.data != null){
+            mselectedImageFileUri = data.data
+
+            try{
+                Glide
+                    .with(this@MyProfileActivity)
+                    .load(mselectedImageFileUri)
+                    .centerCrop()
+                    .placeholder(R.drawable.ic_user_place_holder)
+                    .into(iv_profile_user_image)
+            }catch (e: IOException){
+                e.printStackTrace()
+            }
+        }
+
     }
 
     private fun setupActionBar(){
@@ -38,7 +125,7 @@ class MyProfileActivity : BaseActivity() {
             .load(user.image)
             .centerCrop()
             .placeholder(R.drawable.ic_user_place_holder)
-            .into(iv_user_image)
+            .into(iv_profile_user_image)
 
         et_name.setText(user.name)
         et_email.setText(user.email)
