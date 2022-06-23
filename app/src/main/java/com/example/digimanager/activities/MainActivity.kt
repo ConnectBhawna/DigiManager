2f9ab5844cdefa3d0b284be0f4f98234c2a0563e
Bhawna <bhawnabc2002@gmail.com>
2022-06-23 12:58:50 +0530

    Done with Activity

--- app/src/main/java/com/example/digimanager/activities/MainActivity.kt
+++ app/src/main/java/com/example/digimanager/activities/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.digimanager.activities
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import android.view.MenuItem
 import android.widget.Toast
@@ -18,6 +20,10 @@ import kotlinx.android.synthetic.main.app_bar_main.*
 import kotlinx.android.synthetic.main.nav_header_main.*
 
 class MainActivity : BaseActivity(),NavigationView.OnNavigationItemSelectedListener {
+
+    companion object{
+        const val MY_PROFILE_REQUEST_CODE : Int = 11
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -73,10 +79,23 @@ class MainActivity : BaseActivity(),NavigationView.OnNavigationItemSelectedListe
 
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK
+            && requestCode == MY_PROFILE_REQUEST_CODE){
+              FirestoreClass().loadUserData(this)
+        }else{
+            Log.e("Cancelled","Cancelled")
+        }
+    }
+
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
             R.id.nav_my_profile -> {
-                  startActivity(Intent(this,MyProfileActivity::class.java))
+                  startActivityForResult(
+                      Intent(this,MyProfileActivity::class.java),
+                      MY_PROFILE_REQUEST_CODE
+                  )
             }
             R.id.nav_sign_out ->{
                 FirebaseAuth.getInstance().signOut()
--- app/src/main/java/com/example/digimanager/activities/MyProfileActivity.kt
+++ app/src/main/java/com/example/digimanager/activities/MyProfileActivity.kt
@@ -224,6 +224,7 @@ class MyProfileActivity : BaseActivity() {
 
     fun profileUpdateSuccess() {
         hideProgressDialog()
+        setResult(Activity.RESULT_OK)
         finish()
     }
 
