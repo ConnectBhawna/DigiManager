747b3e68dc7998e7efd383cc78bb9a58344460ca
Bhawna <bhawnabc2002@gmail.com>
2022-07-05 22:35:20 +0530

    working

---
+++ app/src/main/java/com/example/digimanager/adapters/BoardItemsAdapters.kt
@@ -0,0 +1,68 @@
+package com.example.digimanager.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.digimanager.R
+import com.example.digimanager.models.Board
+import kotlinx.android.synthetic.main.item_board.view.*
+
+open class BoardItemsAdapter(
+    private val context: Context,
+    private var list: ArrayList<Board>
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    private var onClickListener: OnClickListener? = null
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+
+        return MyViewHolder(
+            LayoutInflater.from(context).inflate(
+                R.layout.item_board,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val model = list[position]
+
+        if (holder is MyViewHolder) {
+
+            Glide
+                .with(context)
+                .load(model.image)
+                .centerCrop()
+                .placeholder(R.drawable.ic_board_place_holder)
+                .into(holder.itemView.iv_board_image)
+
+            holder.itemView.tv_name.text = model.name
+            holder.itemView.tv_created_by.text = "Created By : ${model.createdBy}"
+
+            holder.itemView.setOnClickListener {
+
+                if (onClickListener != null) {
+                    onClickListener!!.onClick(position, model)
+                }
+            }
+        }
+    }
+
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    fun setOnClickListener(onClickListener: OnClickListener) {
+        this.onClickListener = onClickListener
+    }
+
+    interface OnClickListener {
+        fun onClick(position: Int, model: Board)
+    }
+
+    private class MyViewHolder(view: View) : RecyclerView.ViewHolder(view)
+}
